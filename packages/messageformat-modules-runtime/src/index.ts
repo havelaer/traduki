7a320d4a240e9426c528866c803af6c9fbf25e3f
Max Meinders <max.meinders@gmail.com>
2020-07-10 18:27:44 +0200

    chore: dummy runtime package

--- packages/messageformat-modules-runtime/src/index.ts
+++ packages/messageformat-modules-runtime/src/index.ts
@@ -1,3 +1,7 @@
-export default () => {
-    console.log('messageformat-modules-runtime')
+class Runtime {
+    register(map: Record<string, string>) {
+        console.log(map);
+    }
 }
+
+export default new Runtime();
