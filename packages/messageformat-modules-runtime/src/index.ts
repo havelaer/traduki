7c349ec3a5b4e73d3f13408fa0a0eb3e503aa135
Max Meinders <max.meinders@gmail.com>
2020-07-17 17:29:56 +0200

    feat: inital runtime

--- packages/messageformat-modules-runtime/src/index.ts
+++ packages/messageformat-modules-runtime/src/index.ts
@@ -1,6 +1,35 @@
 class Runtime {
+    private maps: Record<string, string>[] = [];
+    private translations: Record<string, (arg: Record<string, string>) => string> = {};
+    private language: string = '';
+
     register(map: Record<string, string>) {
-        console.log(map);
+        this.maps.push(map);
+    }
+
+    setLocale(language: string) {
+        this.language = language;
+
+        return this;
+    }
+
+    async load() {
+        const language = this.language;
+        const results = await Promise.all(
+            this.maps
+                .map(map => map[language])
+                .filter(Boolean)
+                .map(src => import(src).then(({ default: translations }) => translations),
+            ),
+        );
+        this.translations = results.reduce(
+            (prev, translations) => ({ ...prev, ...translations }),
+            {},
+        );
+    }
+
+    translate(key: string, args: Record<string, string>) {
+        return this.translations[key](args);
     }
 }
 
