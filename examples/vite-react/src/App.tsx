3ea87ee2d71e2d1b3c838d5154f9bafe97f788c2
Max Meinders <max.meinders@gmail.com>
2020-07-22 12:38:16 +0200

    chore: indenting

--- examples/vite-react/src/App.tsx
+++ examples/vite-react/src/App.tsx
@@ -1,38 +1,40 @@
-import React, { useState } from 'react'
-import { useTranslations, useLocale } from '@lazy-lion/react'
+import React, { useState } from 'react';
+import { useTranslations, useLocale } from '@lazy-lion/react';
 import messages from './App.messages.yaml';
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const t = useTranslations();
-  const [, setLocale] = useLocale();
+    const [count, setCount] = useState(0);
+    const t = useTranslations();
+    const [, setLocale] = useLocale();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{t(messages.welcome)}</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>{t(messages.count, { count })}</button>
-        </p>
-        <p dangerouslySetInnerHTML={{ __html: t.markdown(messages.edit) }} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {t(messages.learn)}
-        </a>
-        <p>
-          <button onClick={() => setLocale('en')}>en</button>
-          <button onClick={() => setLocale('nl')}>nl</button>
-        </p>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>{t(messages.welcome)}</p>
+                <p>
+                    <button onClick={() => setCount(count => count + 1)}>
+                        {t(messages.count, { count })}
+                    </button>
+                </p>
+                <p dangerouslySetInnerHTML={{ __html: t.markdown(messages.edit) }} />
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {t(messages.learn)}
+                </a>
+                <p>
+                    <button onClick={() => setLocale('en')}>en</button>
+                    <button onClick={() => setLocale('nl')}>nl</button>
+                </p>
+            </header>
+        </div>
+    );
 }
 
-export default App
+export default App;
