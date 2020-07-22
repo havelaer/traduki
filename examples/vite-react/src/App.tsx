1f737a69b7bca799583b47a6797cb46abbb0cd5f
Max Meinders <max.meinders@gmail.com>
2020-07-22 12:09:26 +0200

    feat: added locale toggle to example

--- examples/vite-react/src/App.tsx
+++ examples/vite-react/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useTranslations } from '@lazy-lion/react'
+import { useTranslations, useLocale } from '@lazy-lion/react'
 import messages from './App.messages.yaml';
 import logo from './logo.svg'
 import './App.css'
@@ -7,18 +7,17 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const t = useTranslations();
+  const [, setLocale] = useLocale();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{t(messages.hello)}</p>
+        <p>{t(messages.welcome)}</p>
         <p>
           <button onClick={() => setCount(count => count + 1)}>{t(messages.count, { count })}</button>
         </p>
-        <p>
-          {t(messages.edit)}
-        </p>
+        <p dangerouslySetInnerHTML={{ __html: t.markdown(messages.edit) }} />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -27,6 +26,10 @@ function App() {
         >
           {t(messages.learn)}
         </a>
+        <p>
+          <button onClick={() => setLocale('en')}>en</button>
+          <button onClick={() => setLocale('nl')}>nl</button>
+        </p>
       </header>
     </div>
   )
