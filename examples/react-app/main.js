89e5e621ed88c484d41c71f81bc71b6ae4b64745
Max Meinders <max.meinders@gmail.com>
2021-08-04 12:49:15 +0200

    chore: remove built files

--- examples/react-app/main.js
+++
@@ -1,7 +0,0 @@
-import { TradukiProvider } from '@traduki/preact';
-import { createElement as h } from 'preact/compat';
-import { render } from 'preact';
-import App from './App';
-import './index.css';
-render(h(TradukiProvider, { initialLocale: "en-US" },
-    h(App, null)), document.getElementById('root'));
